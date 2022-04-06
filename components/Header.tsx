3517d949818bd30c85406d6b066a0fdc554ee17a
gosrahul21 <gosrahul21@gmail.com>
2022-04-06 16:58:15 +0530

    metawebinu

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+
+const Header = (): JSX.Element =>{
+    return (
+        <div className="w-full top-0 sticky text-sky-50 bg-gray-800 py-4 px-2 ">
+            Header
+        </div>
+    )
+}
+
+export default Header;
---
+++ pages/home.tsx
@@ -0,0 +1,16 @@
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+
+const Home = ()=>{
+    return (
+        <div>
+            <Header/>
+            <Banner/>
+            <main>
+
+            </main>
+        </div>
+    )
+}
+
+export default Home;
