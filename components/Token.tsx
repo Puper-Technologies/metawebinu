f8acc778a73dd328e4b23d7ff3e875699f8ce91b
Shubhojeet Banerjee <shubhojeet@gmail.com>
2022-04-08 17:24:37 +0530

    Right Side Token Distribution

--- components/Token.tsx
+++ components/Token.tsx
@@ -1,17 +1,31 @@
-
-function Token (){
-    return <section className="bg-[#0b0c22] h-20">
-        <div className="px-[10%] py-4">
-            {/* left */}
-            <div>
-    
-            </div>
-            {/* right */}
-            <div>
-
-            </div>
+function Token() {
+  return (
+    <section className="container bg-[#0b0c22]">
+      <div className="px-[10%] py-4 text-white">
+        {/* left */}
+        <div></div>
+        {/* right */}
+        <div>
+          <h1 className="text-4xl">
+            Total Distribution Section <br /> End <b>350/350 </b>
+          </h1>
+          ICO Crypto token will be released on the basis of Ethereum and Bitocin
+          platform.
+          <div className="block p-6 rounded-lg shadow-lg bg-white max-w-md mt-5">
+            <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">
+              Card title
+            </h5>
+            <button
+              type="button"
+              className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            >
+              Button
+            </button>
+          </div>
         </div>
+      </div>
     </section>
+  );
 }
 
 export default Token;
